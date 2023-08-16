2c96c2a4f8ea4d6468a5a32541a9b1fcfb1eae77
Rémi Beaufils <remi.beaufils@gmail.com>
2023-08-16 17:27:45 +0200

    use types

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import * as tokenJson from './assets/MyToken.json';
+import { MyToken } from './assets/typechain-types/contracts/ERC20Votes.sol/MyToken';
+import { MyToken__factory } from './assets/typechain-types/factories/contracts/ERC20Votes.sol/MyToken__factory';
 import { ethers } from 'ethers';
 
 const CONTRACT_ADDRESS = '0x208F75C3A395Ad125D0D641D9a2648F837a58538';
 
 @Injectable()
 export class AppService {
-  contract: ethers.Contract;
+  contract: MyToken;
   provider: ethers.JsonRpcProvider;
   wallet: ethers.Wallet;
 
   constructor() {
     this.provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL);
     this.wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, this.provider);
-    this.contract = new ethers.Contract(
-      CONTRACT_ADDRESS,
-      tokenJson.abi,
-      this.wallet,
-    );
+    this.contract = MyToken__factory.connect(CONTRACT_ADDRESS, this.wallet);
   }
 
   getHello(): string {
